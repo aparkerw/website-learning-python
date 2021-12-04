81c9a4cc16ad010d32041bf1016d555c96b25420
Adam Parker <aparkerw@gmail.com>
2021-12-03 23:33:53 -0500

    add flask

---
+++ flask/README.py
@@ -0,0 +1,16 @@
+# website-learning-python flask
+
+## Setup
+
+Install flask `pip3 install flask`
+
+
+
+## running the site
+
+`cd django; python manage.py runserver`
+
+
+## Learning materials
+
+[flask](https://www.youtube.com/watch?v=dam0GPOAvVI)
